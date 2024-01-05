c645e70f8a8f599f0e96b6fe8973b85d59d03a7e
Dagi121 <dagimsime121@gmail.com>
2024-01-05 23:46:40 +0300

    css

---
+++ src/new.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Madegoal(){
+    return <p>shoot on trget</p>
+}
+function Missedgoal(){
+    return <p>shoot off target</p>
+}
+function Sofia(){
+    const x = false;
+return(
+x ? <Madegoal /> : <Missedgoal />
+)
+// if (x){
+//     return <h1>madegoal</h1>
+// }
+// {
+//     return <h1>missed</h1>
+// }
+// const Martiye = (x) =>{
+//    alert(x)
+// }
+
+//return(
+
+    //<button onClick={() => Martiye("Goal!")}>Take the shot!</button>
+//)
+}
+export default Sofia;
