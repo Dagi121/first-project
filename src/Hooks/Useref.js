a8e888158e814ddc508625ff8740b272d287cf05
Dagi121 <dagimsime121@gmail.com>
2024-01-07 21:30:43 +0300

    Gena

--- src/Hooks/Useref.js
+++ src/Hooks/Useref.js
@@ -1,21 +1,29 @@
-import { useRef } from "react";
+import { useState } from "react";
 import ReactDOM from "react-dom/client";
+import Todos from "./Todos";
 
-function App() {
-  const inputElement = useRef();
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
 
-  const focusInput = () => {
-    inputElement.current.focus();
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = () => {
+    setTodos((t) => [...t, "New Todo"]);
   };
 
   return (
     <>
-      <input type="text" ref={inputElement} />
-      <button onClick={focusInput}>Focus Input</button>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
     </>
   );
-}
-
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
