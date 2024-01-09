1dec967e7c942c8678d2a6e58621802f8e9a52b0
Dagi121 <dagimsime121@gmail.com>
2024-01-09 21:56:57 +0300

    third

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
 import './App.css';
-import Usestateobject from './Hooks/Usestateobject';
+import Timer from './Hooks/UseEffect';
+
 
 function App() {
   return (
     <div className="App">
-      <Usestateobject />
+      <Timer />
     </div>
   );
 }
